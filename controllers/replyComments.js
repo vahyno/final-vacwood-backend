e3ae6b95f10f2ba2094d237348f0c3e87dc622f1
vahyno <vahyno@gmail.com>
2018-05-28 22:05:39 -0700

    updated routes, created replyComments.js

---
+++ controllers/replyComments.js
@@ -0,0 +1,70 @@
+var models = require('../models');
+var Comment = models.Comment;
+var Classroom = models.Classroom;
+
+function show(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment){
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(comment.comments)
+        }
+    });
+}
+
+function create(req, res) {
+    Comment.create(req.body, function(err, replyToComment){
+        if (err) {
+            res.send(err);
+        } else {
+            Comment.findById(req.params.comment_id, function(err, comment){
+                if (err) {
+                    res.send(err);
+                } else {
+                    comment.comments.push(replyToComment);
+                    comment.save();
+
+                    Classroom.findById(req.params.classroom_id, function(err, classroom){
+                        if (err) { 
+                            res.send(err);
+                        } else {
+                            var commentToUpdate = classroom.comments.id(req.params.comment_id);
+                            commentToUpdate.comments.push(replyToComment);
+                            classroom.save();
+                            res.json(classroom);
+                        }
+                    });
+                }
+            });
+        }
+    });
+}
+
+function destroy(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment){
+        if (err) {
+            res.send(err);
+        } else {
+            var commentReply = comment.comments.id(req.params.replyComment_id);
+            commentReply.remove();
+            comment.save();
+
+            Classroom.findById(req.params.classroom_id, function(err, classroom){
+                if (err) {
+                    res.send(err);
+                } else {
+                    var commentToUpdate = classroom.comments.id(req.params.comment_id);
+                    var commentToRemove = commentToUpdate.comments.id(req.params.replyComment_id);
+                    commentToRemove.remove();
+                    classroom.save();
+                    res.send(classroom);
+                }
+            });
+        }
+    });
+
+}
+
+module.exports.show = show;
+module.exports.create = create;
+module.exports.destroy = destroy;
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 
 var classroomsController = require('./controllers/classrooms.js');
 var commentsController = require('./controllers/comments.js');
+var replyCommentsController = require('./controllers/replyComments.js');
 
 
 
@@ -47,6 +48,10 @@ app.put('/api/classrooms/:classroom_id/comments/:comment_id', commentsController
 app.delete('/api/classrooms/:classroom_id/comments/:comment_id', commentsController.destroy);
 
 //Response on Comment Routes
+app.get('/api/classrooms/:classroom_id/comments/:comment_id/comments', replyCommentsController.show);
+app.post('/api/classrooms/:classroom_id/comments:comment_id/comments', replyCommentsController.create);
+app.delete('/api/classrooms/:classroom_id/comments/:comment_id/comments/:replyComment_id', replyCommentsController.destroy);
+
 
 // server start
 let port = process.env.PORT || 3001;
