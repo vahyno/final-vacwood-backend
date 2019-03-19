961de8593944c736c800b1e8d77764266d2dbc2c
vahyno <vahyno@gmail.com>
2019-03-18 21:03:40 -0700

    update dependencies

--- models/index.js
+++ models/index.js
@@ -4,7 +4,7 @@ var dbName = 'mongodb://localhost/final_school_community';
 // Map Global Promise to Resolve Mongo Promise Warning
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI || dbName )
+mongoose.connect(process.env.MONGODB_URI || dbName, { useNewUrlParser: true })
 	.then(() => console.log('Mongodb connected...'))
 	.catch(err => console.log(err));
 
